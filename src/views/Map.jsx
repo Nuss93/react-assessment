3bcdfe90cd5a81ee73684310277692b3c7de4ee6
Nuss93 <nasreenrzk@gmail.com>
2023-04-03 23:03:29 +0800

    ADDED REACT PLACES AUTOCOMPLETE PACKAGE

--- src/views/Map.jsx
+++ src/views/Map.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import { Typography } from 'antd'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
+const { Title } = Typography
 export default class Map extends Component {
   render() {
     return (
-      <div>Map</div>
+      <div>
+        <Title>Map</Title>
+        <GooglePlacesAutocomplete />
+      </div>
     )
   }
 }
